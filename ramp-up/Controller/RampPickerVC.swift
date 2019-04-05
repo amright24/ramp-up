da3d147f57e674542ef1c0e7abeda5dfbeacc43e
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-04-05 17:09:15 -0400

    first commit

---
+++ ramp-up/Controller/RampPickerVC.swift
@@ -0,0 +1,49 @@
+//
+//  RampPickerVC.swift
+//  ramp-up
+//
+//  Created by Austin Rightnowar on 4/5/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+    
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.scale = SCNVector3Make(0.0012, 0.0012, 0.0012)
+        node?.position = SCNVector3Make(0.0, 1, -1)
+        scene.rootNode.addChildNode(node!)
+        preferredContentSize = size
+    }
+    
+
+}
